d05edd1b37f6ce46c5f6aafd93658b976ac1fd99
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2023-07-26 11:06:44 +0530

    Create SupplierExample.java

---
+++ SupplierExample.java
@@ -0,0 +1,19 @@
+package com.biconsumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierExample {
+
+	public static void main (String[] args) {
+		
+		Supplier<Student> studentSupplier=()->{
+			return new Student("Adam",2,3.6,"male",Arrays.asList("swimming","basketball","volleyball"));
+			
+		};
+		Supplier<List<Student>> studentListSupplier=()->StudentDatabase.getAllStudents();
+		System.out.println(studentSupplier.get().toString());
+		System.out.println(studentListSupplier.get().toString());
+	}
+}
